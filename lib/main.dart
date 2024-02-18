f49df5235f8528b1c95b43ae4b443c1091d1305e
Alan Barrera <aalan.eduardo@gmail.com>
2024-02-17 22:48:43 -0600

    fin_app_push

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/config/local_notifications/local_notifications.dart';
+import 'package:push_app/config/router/app_router.dart';
+
+import 'package:push_app/config/theme/app_theme.dart';
+import 'package:push_app/presentation/bloc/notifications/notifications_bloc.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage( firebaseMessagingBackgroundHandler );
+
+  await NotificationsBloc.initializeFMC();
+  await LocalNotifications.initializeLocalNotifications();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider( create: ( _ ) => NotificationsBloc(
+          requestLocalNotificationPermissions: LocalNotifications.requestPermissionLocalNotifications,
+          showLocalNotification: LocalNotifications.showLocalNotification
+        ) )
+      ],
+
+      child: const MainApp()
+
+    )
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build( BuildContext context ) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      builder: ( context, child ) => HandleNotificationInterations( child: child! ),
+    );
+  }
+}
+
+
+class HandleNotificationInterations extends StatefulWidget {
+
+  final Widget child;
+  const HandleNotificationInterations({
+    super.key,
+    required this.child
+  });
+
+  @override
+  State<HandleNotificationInterations> createState() => _HandleNotificationInterationsState();
+}
+
+class _HandleNotificationInterationsState extends State<HandleNotificationInterations> {
+
+   // It is assumed that all messages contain a data field with the key 'type'
+  Future<void> setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    // If the message also contains a data property with a "type" of "chat",
+    // navigate to a chat screen
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    // Also handle any interaction when the app is in the background via a
+    // Stream listener
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+  
+  void _handleMessage(RemoteMessage message) {
+
+    context.read<NotificationsBloc>().handleRemoteMessage(message);
+
+    final messageId = message.messageId?.replaceAll( ':', '' ).replaceAll( '%', '' );
+
+    appRouter.push( '/push-details/$messageId' );
+
+    // if (message.data['type'] == 'chat') {
+    //   Navigator.pushNamed(context, '/chat', 
+    //     arguments: ChatArguments(message),
+    //   );
+    // }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Run code required to handle interacted messages in an async function
+    // as initState() must not be async
+    setupInteractedMessage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
